e5511e9c7f17f30c58ef4b2e53aa8484e1be1c28
eldiiar0 <115143883+eldiiar0@users.noreply.github.com>
2023-11-05 19:21:36 +0600

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import TextArea from './TextArea';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Random Text Encoder</h1>
+      <TextArea />
+    </div>
+  );
+}
+
+export default App
